eb8333623a9986b03238d7e98990725357316db7
Zeno <zeno.hamers@gmail.com>
2025-01-28 17:07:54 +0100

    updated scene enum to support scene dependent plugin (capability) selecting

--- misc/scenes_enum.py
+++ misc/scenes_enum.py
@@ -1,17 +1,42 @@
 from enum import Enum
+from typing import List, Optional
+from dataclasses import dataclass
+
+
+DEFAULT_PLUGINS = ["nav", "text", "sql", "image"]
+
+
+@dataclass
+class SceneConfig:
+    name: str
+    plugins: Optional[List[str]] = None
+
+    def __post_init__(self):
+        if self.plugins is None:
+            self.plugins = DEFAULT_PLUGINS
 
 
 class Scene(Enum):
-    APARTMENT_0 = "apartment_0"
-    APARTMENT_1 = "apartment_1"
-    APARTMENT_2 = "apartment_2"
-    ROOM_0 = "room_0"
-    ROOM_1 = "room_1"
-    ROOM_2 = "room_2"
-    OFFICE_0 = "office_0"
-    OFFICE_2 = "office_2"
-    OFFICE_3 = "office_3"
-    OFFICE_4 = "office_4"
-    HOTEL_0 = "hotel_0"
-    FRL_APARTMENT_0 = "frl_apartment_0"
-    FRL_APARTMENT_1 = "frl_apartment_1"
+    TEST_VIRTUAL_SCENE = SceneConfig("test_virtual_scene", ["nav", "text"])
+    APARTMENT_0 = SceneConfig("apartment_0")
+    APARTMENT_1 = SceneConfig("apartment_1")
+    APARTMENT_2 = SceneConfig("apartment_2")
+    ROOM_0 = SceneConfig("room_0")
+    ROOM_1 = SceneConfig("room_1")
+    ROOM_2 = SceneConfig("room_2")
+    OFFICE_0 = SceneConfig("office_0")
+    OFFICE_2 = SceneConfig("office_2")
+    OFFICE_3 = SceneConfig("office_3")
+    OFFICE_4 = SceneConfig("office_4")
+    HOTEL_0 = SceneConfig("hotel_0")
+    FRL_APARTMENT_0 = SceneConfig("frl_apartment_0")
+    FRL_APARTMENT_1 = SceneConfig("frl_apartment_1")
+
+    @property
+    def value(self) -> str:
+        return self._value_.name
+
+    @property 
+    def plugins(self) -> List[str]:
+        return self._value_.plugins
+
