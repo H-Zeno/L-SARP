3f52cf09949d2cc28af24f2ebf7150fe2eee1091
Zeno <zeno.hamers@gmail.com>
2025-02-17 16:11:44 +0100

    update projecting

--- source/scenegraph/projecting.py
+++ source/scenegraph/projecting.py
@@ -3,15 +3,9 @@ import open3d.visualization.rendering as rendering
 import numpy as np
 import json, cv2
 import matplotlib.pyplot as plt
+from .utils import parse_json
 
-def parse_json(file_path):
-    with open(file_path, 'r') as file:
-        data = json.load(file)
-    
-    intrinsics = np.array(data["intrinsics"]).reshape(3, 3)
-    # projection_matrix = np.array(data["projectionMatrix"]).reshape(4, 4)
-    camera_pose = np.array(data["cameraPoseARFrame"]).reshape(4, 4)
-    return intrinsics, camera_pose
+
 
 def draw_box(image, box, width, color=(0, 255, 0), thickness=2):
     for i in range(4):
@@ -78,8 +72,33 @@ def compute_rotation_matrix(normal_dst):
     
     R = np.eye(3) + vx + (vx @ vx) * ((1 - c) / (s ** 2))
     return R
+def project_points_bbox(
+    points_3d: np.ndarray,
+    extrinsics: np.ndarray,
+    intrinsics: np.ndarray,
+    width: int,
+    height: int,
+    bbox: tuple,
+    grid: int = 15
+) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Projects 3D points onto a 2D plane within a specified bounding box, using camera parameters.
 
-def project_points_bbox(points_3d, extrinsics, intrinsics, width, height, bbox, grid=15):
+    This function takes a set of 3D points and projects them onto a 2D image plane, applying specified
+    extrinsic and intrinsic camera parameters. The projection is limited to the defined bounding box
+    and uses a grid to sample points within the bounding box.
+
+    :param points_3d: Array of 3D points to be projected, typically of shape (N, 3).
+    :param extrinsics: 4x4 transformation matrix representing the camera's extrinsic parameters.
+    :param intrinsics: 3x3 matrix representing the camera's intrinsic parameters.
+    :param width: Width of the target 2D projection plane in pixels.
+    :param height: Height of the target 2D projection plane in pixels.
+    :param bbox: Tuple defining the bounding box (xmin, ymin, xmax, ymax) within which to limit the projection.
+    :param grid: Grid size for sampling points within the bounding box to compensate for reduced number of 3D points in the given point cloud. Defaults to 15.
+    :return: Tuple containing:
+        - valid_image_points: 2D numpy array of points within the bounding box on the 2D plane.
+        - valid_points_3d: 3D numpy array of the corresponding 3D points for each valid 2D point.
+    """
     extrinsics = np.linalg.inv(extrinsics)
     
     R = extrinsics[:3, :3]
@@ -155,7 +174,19 @@ def project_points_bbox(points_3d, extrinsics, intrinsics, width, height, bbox, 
     
     return valid_image_points, valid_points_3d
 
-def detections_to_bboxes(points, detections, threshold=0.5):
+def detections_to_bboxes(
+    points: np.ndarray,
+    detections: list,
+    threshold: float = 0.7
+) -> list:
+    """
+    Lifts 2D detection bounding boxes to 3D bounding boxes with confidence scores.
+
+    :param points: Array of 3D points associated with the captured scene of shape (N, 3).
+    :param detections: List of 2D detections.
+    :param threshold: Confidence threshold for considering a detection valid. Defaults to 0.7.
+    :return: List of tuples containing 3D bounding boxes for detections and their associated confidence scores.
+    """
     bboxes_3d = []
     for file, _, confidence, bbox in detections:
         intrinsics, extrinsics = parse_json(file + ".json")
